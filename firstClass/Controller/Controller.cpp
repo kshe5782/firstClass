9537297a02ef9d7daafe97f94dae234477f061aa
Shepherd <kshe5782@740s-pl-112.local>
2019-01-24 08:30:41 -0700

    added controller classes

---
+++ firstClass/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  firstClass
+//
+//  Created by Shepherd, Kayden on 1/24/19.
+//  Copyright © 2019 Ctec. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ firstClass/Controller/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  firstClass
+//
+//  Created by Shepherd, Kayden on 1/24/19.
+//  Copyright © 2019 Ctec. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+#endif /* Controller_hpp */
