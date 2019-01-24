c4273a04115094262d7790e9eb91c0a440946faf
Shepherd <kshe5782@740s-pl-112.local>
2019-01-24 08:24:00 -0700

    added runner

---
+++ firstClass/Runner.cpp
@@ -0,0 +1,14 @@
+//
+//  Runner.cpp
+//  firstClass
+//
+//  Created by Shepherd, Kayden on 1/24/19.
+//  Copyright © 2019 Ctec. All rights reserved.
+//
+
+int main()
+{
+    
+    
+    return 0;
+}
