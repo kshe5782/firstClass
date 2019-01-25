8e5ee4cdf47a326624fc6cb3e5c4749154a4f5ee
Shepherd <kshe5782@740s-pl-112.local>
2019-01-25 07:34:18 -0700

    debugging

--- firstClass/Controller/Controller.cpp
+++ firstClass/Controller/Controller.cpp
@@ -7,3 +7,19 @@
 //
 
 #include "Controller.hpp"
+
+Controller :: Controller()
+{
+    
+}
+
+void Controller :: start()
+{
+    cout << "this is epic" << endl;
+    cout << "You like Arby's curly fries?" << endl;
+    cout << "I have a " << specialNumber << " inch weenie" << endl;
+    string input;
+    cout << "how big is yours?" << endl;
+    cin >> input;
+    cout << input << "? That's it?" << endl;
+}
--- firstClass/Controller/Runner.cpp
+++ firstClass/Controller/Runner.cpp
@@ -5,10 +5,13 @@
 //  Created by Shepherd, Kayden on 1/24/19.
 //  Copyright © 2019 Ctec. All rights reserved.
 //
-
+#include "Controller.hpp"
 int main()
 {
-    
+    Controller myFirstController;
+    myFirstController.start();
+    Controller * myPointer = new Controller();
+    myPointer->start();
     
     return 0;
 }
